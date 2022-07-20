e305b8f79de41f1096149edbc784402cf9ef8581
Cristian Neely <cristianneely@gmail.com>
2022-07-20 19:16:46 -0400

    se puede elegir el color

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ class Pixel {
     this.char = "░░";
     this.style = " ";
   }
-  paint() {
+  paint(color) {
     this.color = true;
     this.char = "██";
-    this.style = "pintado";
+    this.style = color;
   }
   erase() {
     this.style = "";
@@ -20,6 +20,7 @@ class Canva {
     this.width = width;
     this.pixels = new Array();
     this.cursorPressed = false;
+    this.color = "pintado";
 
     for (let i = 0; i < height; i++) {
       const element = new Array(width);
@@ -80,12 +81,12 @@ class Canva {
         
         p.addEventListener("mousemove", () => {
           if (this.cursorPressed) {
-            this.pixels[i][j].paint();
+            this.pixels[i][j].paint(this.color);
           }
         });
         p.addEventListener("mousedown", () => {
           this.cursorPressed = true;
-          this.pixels[i][j].paint();
+          this.pixels[i][j].paint(this.color);
           this.renderHtml();
         });
 
@@ -100,7 +101,22 @@ class Canva {
   }
 }
 
-let Tela = new Canva(40, 40);
+
+
+
+let Tela = new Canva(80, 80);
 Tela.inicializar();
+
+const c1 = document.querySelector(".color1");
+console.log(c1);
+c1.addEventListener("click", () => {
+  Tela.color="pintado"
+});
+const c2= document.querySelector(".color2");
+console.log(c1);
+c2.addEventListener("click", () => {
+  Tela.color="pintado2"
+});
+
 Tela.renderHtml();
 Tela.becomeClickable();
